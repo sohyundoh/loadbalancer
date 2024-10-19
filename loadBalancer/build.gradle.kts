9af499ab3f30c2e9f948a612c4720679078297ca
sohyundoh <lyny.doh@gmail.com>
2024-10-19 12:30:25 +0900

    [feat] 동일한 요청에 대해 응답 시간 최적화를 위한 Cache 사용

--- loadBalancer/build.gradle.kts
+++ loadBalancer/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-
+    implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
 }
 
