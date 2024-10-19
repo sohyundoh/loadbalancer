07270ab4063fcc7733987b5ca720c13a6455f633
sohyundoh <lyny.doh@gmail.com>
2024-10-19 12:30:08 +0900

    [feat] 동일한 요청에 대해 응답 시간 최적화를 위한 Cache 사용

--- loadBalancer/src/main/kotlin/config/CacheConfig.kt
+++ loadBalancer/src/main/kotlin/config/CacheConfig.kt
@@ -1,4 +1,28 @@
 package com.loadbalancer.config
 
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@EnableCaching
+@Configuration
 class CacheConfig {
+
+    @Bean
+    fun redisCacheConfig(): RedisCacheConfiguration {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(5))
+            .disableCachingNullValues()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer())
+            )
+    }
 }
--- loadBalancer/src/main/kotlin/config/RedisConfig.kt
+++ loadBalancer/src/main/kotlin/config/RedisConfig.kt
@@ -1,4 +1,24 @@
 package com.loadbalancer.config
 
-class RedisConfig {
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}")
+    val host: String,
+    @Value("\${spring.data.redis.port}")
+    val port: Int
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(
+            RedisStandaloneConfiguration(host, port)
+        )
+    }
 }
